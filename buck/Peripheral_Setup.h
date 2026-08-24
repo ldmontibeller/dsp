9e8d19d151c616382d86b13cba32a902b19e7ddf
ldmontibeller <ldmontibeller@gmail.com>
2026-08-24 09:55:38 -0300

    working

---
+++ buck/Peripheral_Setup.h
@@ -0,0 +1,18 @@
+/*
+ * Peripheral_Setup.h
+ *
+ *  Created on: 23 de jul de 2020
+ *      Author: waner
+ */
+
+#ifndef PERIPHERAL_SETUP_H_
+#define PERIPHERAL_SETUP_H_
+#include "F28x_Project.h"
+#include "F2837xD_device.h"
+
+void Setup_GPIO(void);
+void Setup_PWM1(void);
+void Setup_ADC(void);
+
+
+#endif /* PERIPHERAL_SETUP_H_ */
